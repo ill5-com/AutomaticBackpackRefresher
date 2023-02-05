3be821e163adaad38d98fe8d885b4c888f991f0f
Ill5.com Softworks <105583568+ill5-com@users.noreply.github.com>
2023-02-05 13:59:59 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,40 +2,110 @@ using Newtonsoft.Json;
 using System;
 using System.IO;
 using System.Net;
+using System.Diagnostics;
 using System.Threading;
+using System.Runtime.CompilerServices;
 
 namespace AutomaticBackpackRefresher
 {
+    public class APIResponse
+    {
+        public int? current_time { get; set; }
+        public int? last_update { get; set; }
+        public int? timestamp { get; set; }
+        public int? next_update { get; set; }
+        public int? refresh_interval { get; set; }
+    }
+
     public class UserConfig
     {
         public string steamId64 { get; set; }
         public string backpackToken { get; set; }
-        public int requestDelay { get; set; }
-        public UserConfig(string _steamId64, string _backpackToken, int _requestDelay = 300000)
+        public UserConfig(string _steamId64, string _backpackToken)
         {
             steamId64 = _steamId64;
             backpackToken = _backpackToken;
-            requestDelay = _requestDelay;
         }
     }
     internal class Program
     {
         public static WebClient webClient = new WebClient();
+        public static APIResponse lastResponse;
         public static UserConfig userConfig;
         public static readonly string configFile = "config.json";
+
+        [MethodImpl(MethodImplOptions.NoInlining)] // https://stackoverflow.com/a/2652481
+        static string GetMethodName(int frameIndex)
+        {
+            var st = new StackTrace();
+            var sf = st.GetFrame(frameIndex);
+
+            return sf.GetMethod().Name;
+        }
+
+        static void DebugPrint(string message)
+        {
+            // Get two methods up in the call stack since we use up one call here
+            Console.WriteLine($"{GetMethodName(2)}: {message}");
+        }
+
+        static void SleepUntilUnixTimestamp(int timestamp) // BUG: 2038
+        {
+            int secondsToSleep = timestamp - Convert.ToInt32(DateTimeOffset.Now.ToUnixTimeSeconds());
+
+            if (secondsToSleep <= 0)
+                return;
+
+            DebugPrint($"Sleeping for {secondsToSleep} seconds...");
+
+            Thread.Sleep(secondsToSleep * 1000);
+        }
+
+        static void ParseAPIException(Exception exception)
+        {
+            // TODO: implement this
+            DebugPrint(exception.ToString());
+        }
+
+        static void ParseAPIResponse(string apiResponse)
+        {
+            APIResponse newResponse;
+            try
+            {
+                newResponse = JsonConvert.DeserializeObject<APIResponse>(apiResponse);
+            }
+            catch (Exception ex)
+            {
+                DebugPrint(ex.ToString());
+                return;
+            }
+
+            if (newResponse.last_update == newResponse.timestamp)
+            {
+                DebugPrint("Refreshed!");
+            }
+
+            lastResponse = newResponse;
+        }
+
         static void RefreshBackpack()
         {
-            Console.WriteLine("Sending refresh request...");
+            DebugPrint("Sending refresh request...");
 
             try
             {
-                webClient.UploadString($"https://backpack.tf/api/inventory/{userConfig.steamId64}/refresh?token={userConfig.backpackToken}", string.Empty);
+                string webResponse = webClient.UploadString($"https://backpack.tf/api/inventory/{userConfig.steamId64}/refresh?token={userConfig.backpackToken}", string.Empty);
+                ParseAPIResponse(webResponse);
             }
-            catch
+            catch (Exception ex)
             {
-                // No care me!
+                ParseAPIException(ex);
+                return;
             }
+
+            DebugPrint("Sent!");
         }
+
         static void GetConfigFromCommandLine()
         {
             Console.Write("Please enter your SteamID64: ");
@@ -46,6 +116,7 @@ namespace AutomaticBackpackRefresher
 
             userConfig = new UserConfig(steamId64, backpackToken);
         }
+
         static void LoadConfig()
         {
             if (!File.Exists(configFile))
@@ -57,19 +128,24 @@ namespace AutomaticBackpackRefresher
 
             File.WriteAllText(configFile, JsonConvert.SerializeObject(userConfig));
         }
+
         static void Main(string[] args)
         {
             Console.Title = "Automatic Backpack Refresher — www.Ill5.com (c) 2023";
-            Console.WriteLine("Automatic Backpack Refresher — www.Ill5.com (c) 2023\n");
+            DebugPrint("Automatic Backpack Refresher — www.Ill5.com (c) 2023\n");
 
             LoadConfig();
 
-            Console.WriteLine("Started!");
+            DebugPrint("Started!");
 
             while (true)
             {
                 RefreshBackpack();
-                Thread.Sleep(userConfig.requestDelay);
+
+                if (lastResponse?.next_update.HasValue == false)
+                    continue;
+
+                SleepUntilUnixTimestamp(lastResponse.next_update.Value);
             }
         }
     }
